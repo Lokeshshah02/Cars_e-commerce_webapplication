baa5570879beb6a5d16b3ea21c884481872ca58f
Lokeshshah02 <lokeshkumarshah112@gmail.com>
2024-03-07 13:07:37 +0530

    search manufacturers

--- carapp/types/index.ts
+++ carapp/types/index.ts
@@ -1,9 +1,15 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title : String;
+    title : string;
     containerStyles? :string;
     handleClick? :
     MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit" | "reset" | undefined;
+}
+
+export interface searchManufactureProps{
+    manufacturer : string;
+    setManufacturer :(manufacturer:string)=> void;
+
 }
