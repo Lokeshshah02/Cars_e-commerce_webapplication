1f1ca2d05909dafbe49f34eeacf7c4f320808cd5
Lokeshshah02 <lokeshkumarshah112@gmail.com>
2024-03-11 21:50:30 +0530

    searchbar

--- carapp/components/SearchBar.tsx
+++ carapp/components/SearchBar.tsx
@@ -2,10 +2,17 @@
 
 import { useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
+import Image from 'next/image'
 
+const SearchButton =({otherClasses} : {otherClasses: string}) =>{
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image src="/magnifying-glass.svg" alt="maginifying glass"  width={40} height={40} className="object-contain"/>
+  </button>
+}
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("")
+  const [model, setModel] =useState('')
   const handleSearch = () =>{
     
   }
@@ -15,7 +22,17 @@ const SearchBar = () => {
       <SearchManufacturer
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
-        />      </div>
+        /> 
+        <SearchButton otherClasses="sm:hideen"/>
+        <div className="searchbar_item">
+<Image src="/model-icon.png" width={25} height={25} className="absolute w-[20px] h-[20px] ml-4" alt="car model" />
+
+<input type="text" name="model" value={model} onChange={(e)=> setModel(e.target.value)} placeholder="TIguan" className="searchbar__input"/>
+<SearchButton otherClasses="sm:hideen"/>
+        </div>
+        <SearchButton otherClasses="max-sm:hideen"/>
+
+             </div>
     </form>
   )
 }
